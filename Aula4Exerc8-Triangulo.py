79a7e793c813f6e80e238f7261538befae71245f
extraterreno <115746191+extraterreno@users.noreply.github.com>
2022-11-10 19:59:31 -0300

    Add files via upload

---
+++ Aula4Exerc8-Triangulo.py
@@ -0,0 +1,17 @@
+#[Exercício 8] Escreva um programa que leia o comprimento de três retas e diga ao usuário se
+# elas podem ou não formar um triângulo.
+
+L1 = float(input("Entre com o primeiro lado: "))
+L2 = float(input("Entre com o segundo lado: "))
+L3 = float(input("Entre com o terceiro lado: "))
+
+V1 = (L1 < L2 + L3)
+V2 = (L2 < L1 + L3)
+V3 = (L3 < L1 + L2)
+
+if V1 and V2 and V3:
+    print("FORMA UM TRIANGULO")
+else:
+    print("NAO FORMA")
+
+
